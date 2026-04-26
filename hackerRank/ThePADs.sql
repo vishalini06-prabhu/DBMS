4ad249b03f54766042b7863f1b5baa80c92ba0a1
Vishalini S <vishalini1786@gmail.com>
2026-04-26 13:38:51 +0530

    Add SQL queries for occupations data

---
+++ hackerRank/ThePADs.sql
@@ -0,0 +1,7 @@
+SELECT CONCAT(name,'(',LEFT(occupation,1),')')
+FROM occupations
+ORDER BY name;
+SELECT CONCAT('There are a total of ',COUNT(*),' ',LOWER(occupation),'s.')
+FROM occupations
+GROUP BY occupation
+ORDER BY COUNT(*), occupation;
