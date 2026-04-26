47562e5876502acc2cdf31193ee5d6026ee71f24
Vishalini S <vishalini1786@gmail.com>
2026-04-26 23:50:00 +0530

    Create employee table in SQL

---
+++ CodeChef/createTable.sql
@@ -0,0 +1,6 @@
+CREATE TABLE employee
+(
+    Employee_id Integer , /* Integer Data type assigned to the variable */
+    Employee_Name   TEXT, /* TEXT Data type assigned to the variable */
+    Department  TEXT /* TEXT Data type assigned to the variable */
+);
